9f7a3789386c5d768b0ad97f73baf5047006d7f0
Sergey <sergey.riedel@gmail.com>
2020-10-15 11:21:33 +0200

    Add tests for fetchFeed actions

---
+++ src/actions/fetchFeed.test.js
@@ -0,0 +1,66 @@
+import api from '../api';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import MockAdapter from 'axios-mock-adapter';
+
+import {
+  FETCH_FEED_REQUEST,
+  FETCH_FEED_SUCCESS,
+  FETCH_FEED_FAILURE,
+  fetchFeed,
+} from './fetchFeed';
+
+const middleWare = [thunk];
+const mockStore = configureMockStore(middleWare);
+const fakeAxios = new MockAdapter(api);
+const store = mockStore();
+
+describe('fetchFeed action', () => {
+  beforeEach(() => {
+    store.clearActions();
+  });
+
+  test('dispatches FETCH_FEED_SUCCESS', async () => {
+    fakeAxios.onGet('/feeds').reply(200, {
+      feeds: [
+        { _id: 1, content: 'Some content' },
+        { _id: 2, content: 'Some more content' },
+      ],
+    });
+
+    await store.dispatch(fetchFeed());
+
+    const expectedActions = [
+      { type: FETCH_FEED_REQUEST },
+      {
+        type: FETCH_FEED_SUCCESS,
+        payload: [
+          { _id: 1, content: 'Some content' },
+          { _id: 2, content: 'Some more content' },
+        ],
+      },
+    ];
+
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  test('dispatches FETCH_FEED_FAILURE', async () => {
+    fakeAxios.onGet('/feeds').reply(400, {
+      error: {
+        message: 'Request failed with status code 400',
+      },
+    });
+
+    await store.dispatch(fetchFeed());
+
+    const expectedActions = [
+      { type: FETCH_FEED_REQUEST },
+      {
+        type: FETCH_FEED_FAILURE,
+        payload: new Error('Request failed with status code 400'),
+      },
+    ];
+
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+});
